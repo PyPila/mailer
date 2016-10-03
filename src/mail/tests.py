8ea8226c739067e68fcbe0d713e76e4af20c3a98
Pawel Kucmus <pkucmus@gmail.com>
2016-10-03 21:05:58 +0200

    playing with travis

--- src/mail/tests.py
+++ src/mail/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from mail.models import Email, EmailFields
+from mail.models import Email, EmailFields, Recipient
 
 
 class EmailSendTestCase(TestCase):
@@ -9,15 +9,51 @@ class EmailSendTestCase(TestCase):
     def setUp(self):
         self.email = Email.objects.create(
             name='test',
+            subject='test',
+            from_email='test@pypila.com',
             template=SimpleUploadedFile(
                 'test_template.html',
-                'Test Email: {{ test_context }}'
+                (
+                    'Test Email: {{ test_context }} '
+                    'Recipient: {{ recipient.name }} '
+                )
             )
         )
-        self.email.fields.add(
-            EmailFields.objects.create(name='test_context', value='test_value')
+        self.email.make_template_fields()
+        field = EmailFields.objects.last()
+        field.value = 'some content'
+        field.save()
+        self.recipient = Recipient.objects.create(
+            name='Test Recipient',
+            email='test@pypila.com',
         )
+        self.rf = RequestFactory()
+
+    def test_make_email_fields(self):
+        email = Email.objects.create(
+            name='test',
+            subject='test',
+            from_email='test@pypila.com',
+            template=SimpleUploadedFile(
+                'test_template.html',
+                'Test Email: {{ content }}',
+            )
+        )
+        email.make_template_fields()
+
+        fields = email.fields.all()
+        self.assertEqual(fields.count(), 1)
+        self.assertEqual(fields[0].name, 'content')
 
     def test_render(self):
-        mail_content = self.email.render()
-        self.assertEqual(mail_content, 'Test Email: test_value')
+        mail_content = self.email.render(
+            self.rf.get('/'),
+            self.recipient,
+        )[0]
+        self.assertEqual(
+            mail_content,
+            (
+                'Test Email: some content '
+                'Recipient: Test Recipient '
+            )
+        )
